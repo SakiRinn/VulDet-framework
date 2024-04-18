c1f2ee02c8289605ab89f62afdd4d08c686edee6
SakiRinn <sakirinn@qq.com>
2024-04-18 09:47:49 +0800

    train word2vec

--- word2vec.py
+++ word2vec.py
@@ -2,6 +2,7 @@ import logging
 from gensim.models import Word2Vec
 import json
 import re
+import os
 import os.path as osp
 
 
@@ -33,15 +34,20 @@ def code_tokenize(code):
 
 def train_w2v(sentences, epochs=5, min_count=1, embedding_size=128,
               output_dir='outputs/w2v/'):
-    sentences = [code_tokenize(sentence) for sentence in sentences]
+    words = []
+    for sentence in sentences:
+        words += code_tokenize(sentence)
 
-    logging.info(f'Total sentences: {len(sentences)}')
+    logging.info(f'Total words: {len(words)}')
     logging.info('Training...')
 
-    w2v_model = Word2Vec(sentences, vector_size=embedding_size, min_count=min_count, workers=8)
+    if not osp.exists(output_dir):
+        os.makedirs(output_dir)
+
+    w2v_model = Word2Vec(words, vector_size=embedding_size, min_count=min_count, workers=8)
     for i in range(epochs):
-        w2v_model.train(sentences, total_examples=len(sentences), epochs=1)
-        w2v_model.save(osp.join(output_dir, 'e1.bin'))
+        w2v_model.train(words, total_examples=len(words), epochs=1)
+        w2v_model.save(osp.join(output_dir, f'e{i+1}.bin'))
 
     logging.info('Train completed!')
 
@@ -50,4 +56,4 @@ if __name__ == '__main__':
     with open('data/devign/devign.json', 'r') as f:
         data = json.load(f)
         sentences = [e['func'] for e in data]
-    train_w2v(sentences, output_dir='outputs/w2v/devign.bin')
+    train_w2v(sentences, output_dir='outputs/w2v/')
